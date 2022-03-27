036b8831dc2986c037311ad1d76df9d21b6c7060
GOMATHI KAVI <GOMATHI KAVI@DESKTOP-AB6DJ25>
2022-03-27 03:12:02 -0700

    push into github

---
+++ 10-2-22/src/com/java/Ex1.java
@@ -0,0 +1,18 @@
+package com.java;
+
+public class Ex1 {
+
+	public static void main(String[] args) {
+		int a=5;
+		a++;
+		System.out.println(a);
+		++a;
+		System.out.println(a);
+		a--;
+		System.out.println(a);
+		--a;
+		System.out.println(--a);
+
+	}
+
+}
